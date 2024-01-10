2250dcd9ab19632a869300bc207385e6ec3746b4
Nafeu Nasir <nafeu.nasir@gmail.com>
2024-01-10 00:39:40 -0500

    Added lightbeams to concept-maps

--- tools/commander/concept-maps.js
+++ tools/commander/concept-maps.js
@@ -17,7 +17,153 @@ rand(-300, 300)
 
 #black
 -16777216
+
+#skip
+Rectangle_skip|z:10000;
 `
+
+const backgroundObjects = `
+#prefabs
+[horizon]
+[horizon]&[mountain_split]
+[horizon]&[city]
+[horizon]&[mountain_split]&[city]
+[horizon]&[trees]
+[horizon]&[trees]&[city]
+
+#city
+Flatbox_city|xIter:12600.0;xIterFunc:sin(x);xr:90;y:-5000;yIter:400.0;yIterFunc:random 100;z:-46000;zIter:200.0;zr:45;w:600.0;h:600.0;l:10000.0;lIter:-800.0;lIterFunc:random 100;s:100.0;fillColor:-16777216;i:51;zrIter:10;
+
+#horizon
+Rectangle_horizon_accentafill|y:40000;z:-65000;w:200000.0;h:80000.0;fillColor:[black];
+Rectangle_horizon_nostroke_accentafill|y:40000;z:-65000;w:200000.0;h:80000.0;fillColor:[black];
+
+#mountain_split
+[left_mountain_f]&[right_mountain_b]
+[left_mountain_b]&[right_mountain_f]
+[left_mountain_f]
+[right_mountain_f]
+#left_mountain_f
+Triangle_leftmountain_accentbfill|x:-[left_mountain_growth*a];z:-70000;w:150000.0+[left_mountain_growth*a]+[mountain_w];h:25000.0+[left_mountain_growth];s:100.0;variation:left-right;
+Triangle_leftmountain_accentbfill|x:[left_mountain_growth*a];z:-70000;w:150000.0+[left_mountain_growth*a]*2+[mountain_w];h:25000.0+[left_mountain_growth];s:100.0;variation:left-right;
+#right_mountain_b
+Triangle_rightmountain_accentcfill|x:-[right_mountain_growth*b];z:-72000;w:-150000.0-[right_mountain_growth*b]*2-[mountain_w];h:25000.0+[right_mountain_growth*b];s:100.0;variation:left-right;
+Triangle_rightmountain_accentcfill|x:[right_mountain_growth*b];z:-72000;w:-150000.0-[right_mountain_growth*b]-[mountain_w];h:25000.0+[right_mountain_growth*b];s:100.0;variation:left-right;
+#left_mountain_b
+Triangle_leftmountain_accentbfill|x:-[left_mountain_growth*a];z:-72000;w:150000.0+[left_mountain_growth*a]+[mountain_w];h:25000.0+[left_mountain_growth];s:100.0;variation:left-right;
+Triangle_leftmountain_accentbfill|x:[left_mountain_growth*a];z:-72000;w:150000.0+[left_mountain_growth*a]*2+[mountain_w];h:25000.0+[left_mountain_growth];s:100.0;variation:left-right;
+#right_mountain_f
+Triangle_rightmountain_accentcfill|x:-[right_mountain_growth*b];z:-70000;w:-150000.0-[right_mountain_growth*b]*2-[mountain_w];h:25000.0+[right_mountain_growth*b];s:100.0;variation:left-right;
+Triangle_rightmountain_accentcfill|x:[right_mountain_growth*b];z:-70000;w:-150000.0-[right_mountain_growth*b]-[mountain_w];h:25000.0+[right_mountain_growth*b];s:100.0;variation:left-right;
+#left_mountain_rotation
+rand(8, 42)
+#right_mountain_rotation
+rand(-50, -5)
+#left_mountain_growth
+rand(100, 100000, 'offset_a')
+#right_mountain_growth
+rand(100, 100000, 'offset_b')
+#mountain_w
+rand(100, 100000)
+
+#trees
+Terrain_trees|z:-6000;zIter:930.0;w:13700.0;wIter:-3195.0;h:100.0;s:60.0;paramA:50.0;paramB:350.0;paramC:50.0;paramCIter:-30.0;paramD:50.0;variation:trees;i:3;
+Terrain_trees|y:15;z:-6000;zIter:970.0;w:13700.0;wIter:-3195.0;h:100.0;s:60.0;paramB:610.0;paramCIter:140.0;paramD:135.0;variation:trees;i:3;
+`+sharedValues;
+
+const celestialObjects = `
+#prefabs
+[planet]
+
+#planet
+[body]
+[body]&[shadow]
+[body]&[rings]
+[body]&[shadow]&[rings]
+[skip]
+
+#body
+Circle_planetbody|x:[planet_x*celestial1];y:[planet_y*celestial2];z:-100000;w:100.0;h:100.0;s:[planet_s*celestial3];fillColor:[black];
+#planet_x
+rand(-25000, 25000, 'celestial1')
+#planet_y
+rand(-35000, -7000, 'celestial2')
+#planet_s
+rand(5000, 75000, 'celestial3')
+#shadow
+Circle_planetshadow_nostroke_backgroundfill|x:[planet_x*celestial1];y:[planet_y*celestial2];z:-100000+[shadow_z_offset];w:100.0;h:100.0;s:[planet_s*celestial3]-[shadow_s_offset];
+#shadow_z_offset
+rand(500, 2000)
+#shadow_s_offset
+rand(500, 2000)
+
+#rings
+[ring]
+[ring]&[ring]
+[ring]&[ring]&[ring]
+[ring]&[ring]&[ring]&[ring]
+[ring]&[ring]&[ring]&[ring]&[ring]
+#ring
+Circle_planetring_nofill|x:[planet_x*celestial1];y:[planet_y*celestial2];z:-100000;w:100.0;h:100.0;s:[planet_s*f3]+[ring_s_offset];xr:[ring_xr*celestial4];yr:[ring_xr*celestial5];zr:[ring_xr*celestial6];
+#ring_s_offset
+rand(1000, 20000)
+#ring_xr
+rand(0, 180, 'celestial4')
+#ring_yr
+rand(0, 180, 'celestial5')
+#ring_zr
+rand(0, 180, 'celestial6')
+`+sharedValues;
+
+const skyObjects = `
+#prefabs
+[lightbeams]
+
+#lightbeams
+[lightbeam]
+[lightbeam]&[lightbeam]
+[skip]
+
+#lightbeam
+Line_lightbeam_nofill_accentdstroke|x:[lightbeam_x];y:-[lightbeam_y];z:-99999;w:300000.0;h:100.0;zr:[lightbeam_r];
+
+#lightbeam_r
+rand(-10, 10)
+
+#lightbeam_x
+rand(-20000, 20000)
+
+#lightbeam_y
+rand(8000, 30000)
+`+sharedValues;
+
+const surroundingObjects = `
+#prefabs
+[water]&[waves]
+[waves]
+[water]
+[floor]
+[skip]
+
+#water
+Rectangle_water|xr:90;y:[water_level];yMod:[tide];yModType:sine;z:-20000;w:100000.0;h:60000.0;fillColor:0;
+#water_level
+rand(150, 220)
+#tide
+rand(80, 200)
+#waves
+Arc_waves_nofill|xr:90;y:190;z:700;zr:180;w:100.0;wIter:200.0;h:100.0;hIter:200.0;s:100.0;strokeWeight:1.0;paramB:180.0;i:100;zMod:200;zModType:sawtooth;fillColor:0;
+Arc_waves_nofill|xr:90;y:190;z:700;zr:180;w:100.0;wIter:200.0;h:100.0;hIter:200.0;s:100.0;strokeWeight:1.0;paramB:180.0;i:100;fillColor:0;
+Arc_waves_nofill|xr:90;y:190;yIter:-2.0;z:900;zMod:200.0;zModType:sawtooth;zr:180;w:100.0;wIter:200.0;h:100.0;hIter:200.0;s:100.0;strokeWeight:1.0;paramB:180.0;i:100;
+Arc_waves_nofill|xr:90;y:190;yIter:-2.0;z:900;zr:180;w:100.0;wIter:200.0;h:100.0;hIter:200.0;s:100.0;strokeWeight:1.0;paramB:180.0;i:100;
+Arc_waves_nofill|xr:90;y:91;z:1000;zMod:200.0;zModType:sawtooth;zr:180;w:100.0;wIter:400.0;h:100.0;hIter:400.0;s:100.0;strokeWeight:1.0;paramB:180.0;i:100;
+Arc_waves_nofill|xr:90;y:400;yIter:-4.0;z:1001;zMod:200.0;zModType:sawtooth;zr:180;w:100.0;wIter:400.0;h:100.0;hIter:400.0;s:100.0;strokeWeight:1.0;paramB:180.0;i:100;
+
+#floor
+Rectangle_floor_accentcfill|xr:90;y:[floor_y];z:-20000;w:100000.0;h:60000.0;fillColor:0;
+#floor_y
+rand(50, 500)
+`+sharedValues;
 
 const foregroundObjects = `
 #prefabs
@@ -25,6 +171,7 @@ const foregroundObjects = `
 [square_arch]
 [poles]
 [turnway]
+[skip]
 
 #turnway
 [turnway_left]&[turnway_right]
@@ -50,11 +197,11 @@ rand(50, 150)
 #poles
 [base_pole]&[pole_tip]
 #base_pole
-Flatbox_basepole_accentafill_accentbstroke|x:[pole_x*a];xr:90;y:200;z:345;zIter:-[pole_ziter*a];zrIter:65.0;zrIterFunc:random 100;w:-50.0;h:[pole_h];l:1110.0;s:100.0;i:100;fillColor:0;xIter:[pole_xiter*a];
-Flatbox_basepole_accentcfill_accentdstroke|x:[pole_x*a];xr:90;y:200;z:345;zIter:-[pole_ziter*a];zrIter:65.0;zrIterFunc:random 100;w:-50.0;h:[pole_h];l:1110.0;s:100.0;i:100;fillColor:0;xIter:[pole_xiter*a];
+Flatbox_basepole_accentafill_accentbstroke|x:[pole_x*a];xr:90;y:200;z:345;zIter:-[pole_ziter*a];zrIter:65.0;zrIterFunc:random 100;w:-50.0;h:[pole_h];l:1110.0;s:100.0;i:75;fillColor:0;xIter:[pole_xiter*a];
+Flatbox_basepole_accentcfill_accentdstroke|x:[pole_x*a];xr:90;y:200;z:345;zIter:-[pole_ziter*a];zrIter:65.0;zrIterFunc:random 100;w:-50.0;h:[pole_h];l:1110.0;s:100.0;i:75;fillColor:0;xIter:[pole_xiter*a];
 #pole_tip
-Flatbox_poletip_accentbfill_accentastroke|x:[pole_x*a];xr:90;y:-415;z:345;zIter:-[pole_ziter*a];zrIter:65.0;zrIterFunc:random 100;w:-50.0;h:[pole_h];l:110.0;s:100.0;i:100;fillColor:0;xIter:[pole_xiter*a];
-Flatbox_poletip_accentdfill_accentcstroke|x:[pole_x*a];xr:90;y:-415;z:345;zIter:-[pole_ziter*a];zrIter:65.0;zrIterFunc:random 100;w:-50.0;h:[pole_h];l:110.0;s:100.0;i:100;fillColor:0;xIter:[pole_xiter*a];
+Flatbox_poletip_accentbfill_accentastroke|x:[pole_x*a];xr:90;y:-415;z:345;zIter:-[pole_ziter*a];zrIter:65.0;zrIterFunc:random 100;w:-50.0;h:[pole_h];l:110.0;s:100.0;i:75;fillColor:0;xIter:[pole_xiter*a];
+Flatbox_poletip_accentdfill_accentcstroke|x:[pole_x*a];xr:90;y:-415;z:345;zIter:-[pole_ziter*a];zrIter:65.0;zrIterFunc:random 100;w:-50.0;h:[pole_h];l:110.0;s:100.0;i:75;fillColor:0;xIter:[pole_xiter*a];
 #pole_h
 rand(50, 300)
 #pole_x
@@ -68,23 +215,23 @@ rand(1, 10, 'xiter')*20
 #square_arch
 [square_arch_a1]&[square_arch_a2]&[square_arch_b1]&[square_arch_b2]&[square_arch_c1]&[square_arch_c2]&[square_arch_top]
 #square_arch_a1
-Flatbox_squarearch_a1_accentdfill_accentastroke|x:-[square_arch_x_offset*a];xr:90;y:100;zIter:-[square_arch_ziter*a];w:50.0;h:[square_arch_a_h*a];l:170.0;s:100.0;fillColor:[black];i:100;z:600;zr:[square_arch_a_zr*a];
+Flatbox_squarearch_a1_accentdfill_accentastroke|x:-[square_arch_x_offset*a];xr:90;y:100;zIter:-[square_arch_ziter*a];w:50.0;h:[square_arch_a_h*a];l:170.0;s:100.0;fillColor:[black];i:50;z:600;zr:[square_arch_a_zr*a];
 #square_arch_a2
-Flatbox_squarearch_a2_accentdfill_accentastroke|x:[square_arch_x_offset*a];xr:90;y:100;zIter:-[square_arch_ziter*a];w:50.0;h:[square_arch_a_h*a];l:170.0;s:100.0;fillColor:[black];i:100;z:600;zr:-[square_arch_a_zr*a];
+Flatbox_squarearch_a2_accentdfill_accentastroke|x:[square_arch_x_offset*a];xr:90;y:100;zIter:-[square_arch_ziter*a];w:50.0;h:[square_arch_a_h*a];l:170.0;s:100.0;fillColor:[black];i:50;z:600;zr:-[square_arch_a_zr*a];
 #square_arch_a_h
 rand(50, 245, 'a_h')
 #square_arch_a_zr
 rand(1, 3, 'a_zr')*15
 #square_arch_b1
-Flatbox_squarearch_b1_accentafill_accentbstroke|x:-[square_arch_x_offset*a];xr:90;y:-55;zIter:-[square_arch_ziter*a];zr:-45;w:25.0;h:35.0;l:145.0;s:100.0;fillColor:[black];i:100;z:600;
+Flatbox_squarearch_b1_accentafill_accentbstroke|x:-[square_arch_x_offset*a];xr:90;y:-55;zIter:-[square_arch_ziter*a];zr:-45;w:25.0;h:35.0;l:145.0;s:100.0;fillColor:[black];i:50;z:600;
 #square_arch_b2
-Flatbox_squarearch_b2_accentafill_accentbstroke|x:[square_arch_x_offset*a];xr:90;y:-55;zIter:-[square_arch_ziter*a];zr:45;w:25.0;h:35.0;l:145.0;s:100.0;fillColor:[black];i:100;z:600;
+Flatbox_squarearch_b2_accentafill_accentbstroke|x:[square_arch_x_offset*a];xr:90;y:-55;zIter:-[square_arch_ziter*a];zr:45;w:25.0;h:35.0;l:145.0;s:100.0;fillColor:[black];i:50;z:600;
 #square_arch_c1
-Flatbox_squarearch_c1_accentbfill_accentcstroke|x:[square_arch_x_offset*a];xr:90;y:-225;zIter:-[square_arch_ziter*a];w:25.0;h:10.0;l:195.0;s:100.0;fillColor:[black];i:100;z:600;
+Flatbox_squarearch_c1_accentbfill_accentcstroke|x:[square_arch_x_offset*a];xr:90;y:-225;zIter:-[square_arch_ziter*a];w:25.0;h:10.0;l:195.0;s:100.0;fillColor:[black];i:50;z:600;
 #square_arch_c2
-Flatbox_squarearch_c2_accentbfill_accentcstroke|x:-[square_arch_x_offset*a];xr:90;y:-225;zIter:-[square_arch_ziter*a];w:25.0;h:10.0;l:195.0;s:100.0;fillColor:[black];i:100;z:600;
+Flatbox_squarearch_c2_accentbfill_accentcstroke|x:-[square_arch_x_offset*a];xr:90;y:-225;zIter:-[square_arch_ziter*a];w:25.0;h:10.0;l:195.0;s:100.0;fillColor:[black];i:50;z:600;
 #square_arch_top
-Flatbox_squarearch_top_accentcfill_accentdstroke|y:-340;yr:90;zIter:-[square_arch_ziter*a];w:[square_arch_top_w];h:25.0;l:425.0+[square_arch_x_offset*a];s:100.0;fillColor:[black];i:100;z:600;
+Flatbox_squarearch_top_accentcfill_accentdstroke|y:-340;yr:90;zIter:-[square_arch_ziter*a];w:[square_arch_top_w];h:25.0;l:425.0+[square_arch_x_offset*a];s:100.0;fillColor:[black];i:50;z:600;
 #square_arch_top_w
 rand(40, 240)
 rand(15, 25)
@@ -94,111 +241,12 @@ rand(195, 400, 'x_offset')
 rand(300, 600, 'ziter')
 `+sharedValues;
 
-const surroundingObjects = `
-#prefabs
-[water]&[waves]
-[waves]
-[water]
-[floor]
-
-#water
-Rectangle_water|xr:90;y:[water_level];yMod:[tide];yModType:sine;z:-20000;w:100000.0;h:60000.0;fillColor:0;
-#water_level
-rand(150, 220)
-#tide
-rand(80, 200)
-#waves
-Arc_waves_nofill|xr:90;y:190;z:500;zr:180;w:100.0;wIter:200.0;h:100.0;hIter:200.0;s:100.0;strokeWeight:1.0;paramB:180.0;i:100;zMod:200;zModType:sawtooth;fillColor:0;
-Arc_waves_nofill|xr:90;y:190;z:500;zr:180;w:100.0;wIter:200.0;h:100.0;hIter:200.0;s:100.0;strokeWeight:1.0;paramB:180.0;i:100;fillColor:0;
-Arc_waves_nofill|xr:90;y:190;yIter:-2.0;z:900;zMod:200.0;zModType:sawtooth;zr:180;w:100.0;wIter:200.0;h:100.0;hIter:200.0;s:100.0;strokeWeight:1.0;paramB:180.0;i:100;
-Arc_waves_nofill|xr:90;y:190;yIter:-2.0;z:900;zr:180;w:100.0;wIter:200.0;h:100.0;hIter:200.0;s:100.0;strokeWeight:1.0;paramB:180.0;i:100;
-Arc_waves_nofill|xr:90;y:91;z:1000;zMod:200.0;zModType:sawtooth;zr:180;w:100.0;wIter:400.0;h:100.0;hIter:400.0;s:100.0;strokeWeight:1.0;paramB:180.0;i:100;
-Arc_waves_nofill|xr:90;y:400;yIter:-4.0;z:1001;zMod:200.0;zModType:sawtooth;zr:180;w:100.0;wIter:400.0;h:100.0;hIter:400.0;s:100.0;strokeWeight:1.0;paramB:180.0;i:100;
-
-#floor
-Rectangle_floor_accentcfill|xr:90;y:[floor_y];z:-20000;w:100000.0;h:60000.0;fillColor:0;
-#floor_y
-rand(50, 500)
-`+sharedValues;
-
-const backgroundObjects = `
-#prefabs
-[horizon]
-[horizon]&[city]
-[horizon]&[mountain_split]&[city]
-[horizon]&[trees]
-[trees]
-
-#city
-Flatbox_city|xIter:12600.0;xIterFunc:sin(x);xr:90;y:-5000;yIter:400.0;yIterFunc:random 100;z:-46000;zIter:200.0;zr:45;w:600.0;h:600.0;l:10000.0;lIter:-800.0;lIterFunc:random 100;s:100.0;fillColor:-16777216;i:51;zrIter:10;
-
-#horizon
-Rectangle_horizon_accentafill|y:40000;z:-95000;w:200000.0;h:80000.0;fillColor:[black];
-Rectangle_horizon_nostroke_accentafill|y:40000;z:-95000;w:200000.0;h:80000.0;fillColor:[black];
-
-#mountain_split
-[left_mountain]&[right_mountain]
-#left_mountain
-Rectangle_leftmountain|y:20000;z:-40991;zr:[left_mountain_rotation];w:200000.0;h:40000.0;fillColor:[black];
-#right_mountain
-Rectangle_rightmountain|y:20000;z:-39967;zr:[right_mountain_rotation];w:200000.0;h:40000.0;fillColor:[black];
-#left_mountain_rotation
-rand(8, 42)
-#right_mountain_rotation
-rand(-50, -5)
-
-#trees
-Terrain_trees|z:-6000;zIter:930.0;w:13700.0;wIter:-3195.0;h:100.0;s:60.0;paramA:50.0;paramB:350.0;paramC:50.0;paramCIter:-30.0;paramD:50.0;variation:trees;i:3;
-Terrain_trees|y:15;z:-6000;zIter:970.0;w:13700.0;wIter:-3195.0;h:100.0;s:60.0;paramB:610.0;paramCIter:140.0;paramD:135.0;variation:trees;i:3;
-`+sharedValues;
-
-const celestialObjects = `
-#prefabs
-[planet]
-
-#planet
-[body]
-[body]&[shadow]
-[body]&[rings]
-[body]&[shadow]&[rings]
-#body
-Circle_planetbody|x:[planet_x*celestial1];y:[planet_y*celestial2];z:-50000;w:100.0;h:100.0;s:[planet_s*celestial3];fillColor:[black];
-#planet_x
-rand(-25000, 25000, 'celestial1')
-#planet_y
-rand(-20000, -7000, 'celestial2')
-#planet_s
-rand(5000, 20000, 'celestial3')
-#shadow
-Circle_planetshadow_nostroke_backgroundfill|x:[planet_x*celestial1];y:[planet_y*celestial2];z:-50000+[shadow_z_offset];w:100.0;h:100.0;s:[planet_s*celestial3]-[shadow_s_offset];
-#shadow_z_offset
-rand(500, 1000)
-#shadow_s_offset
-rand(500, 1000)
-
-#rings
-[ring]
-[ring]&[ring]
-[ring]&[ring]&[ring]
-[ring]&[ring]&[ring]&[ring]
-[ring]&[ring]&[ring]&[ring]&[ring]
-#ring
-Circle_planetring_nofill|x:[planet_x*celestial1];y:[planet_y*celestial2];z:-50000;w:100.0;h:100.0;s:[planet_s*f3]+[ring_s_offset];xr:[ring_xr*celestial4];yr:[ring_xr*celestial5];zr:[ring_xr*celestial6];
-#ring_s_offset
-rand(1000, 5000)
-#ring_xr
-rand(0, 180, 'celestial4')
-#ring_yr
-rand(0, 180, 'celestial5')
-#ring_zr
-rand(0, 180, 'celestial6')
-`+sharedValues;
-
 const conceptMaps = [
-  foregroundObjects,
-  surroundingObjects,
   backgroundObjects,
-  celestialObjects
+  celestialObjects,
+  skyObjects,
+  surroundingObjects,
+  foregroundObjects
 ]
 
 module.exports = { conceptMaps };
